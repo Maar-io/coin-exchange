9a120037ef2da004bc83bc5b08cbd1b6668096d6
Mario <mariovukelic@yahoo.com>
2020-10-20 19:53:17 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Coin from './components/Coin/Coin';
+import AccountBalance from './components/AccountBalance/AccountBalance';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='./polka-dot.png' alt="Polkadot logo" />
+        <h1 className ="App-title">
+          Polkadot Ecosystem prices
+        </h1>
       </header>
+      <AccountBalance amount={10000}/>
+      <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+            <Coin name="Polkadot" ticker="DOT" price={4.2}/>
+            <Coin name="Kusama" ticker="KSM" price={38.4}/>
+            <Coin name="Moonbeam" ticker="MOON" price={8.2}/>
+            <Coin name="Acala" ticker="AKL" price={0.02}/>
+          
+          
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/AccountBalance/AccountBalance.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components'
+
+const Section = styled.section`
+    border: 1px solid red;
+`
+
+export default class AccountBalance extends Component {
+    render() {
+        return (
+            <Section>
+                $ {this.props.amount}
+            </Section>
+        )
+    }
+}
+
+AccountBalance.propTypes = {
+    amount: PropTypes.number.isRequired
+}
