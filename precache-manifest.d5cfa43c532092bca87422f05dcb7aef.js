9fd4a82a11da1486414469cd47536029c5be53c6
Mario <mariovukelic@yahoo.com>
2020-11-18 19:14:10 +0100

    Updates

---
+++ precache-manifest.d5cfa43c532092bca87422f05dcb7aef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68cf3ec29567e9c37aae3a762dee9b3",
+    "url": "/coin-exchange/index.html"
+  },
+  {
+    "revision": "d148b72304e9184dd657",
+    "url": "/coin-exchange/static/css/2.bfffec66.chunk.css"
+  },
+  {
+    "revision": "3a92cb4952557c314959",
+    "url": "/coin-exchange/static/css/main.d34d70fd.chunk.css"
+  },
+  {
+    "revision": "d148b72304e9184dd657",
+    "url": "/coin-exchange/static/js/2.fe136b61.chunk.js"
+  },
+  {
+    "revision": "3ec276135aea9d02acc33f7d4317e5e5",
+    "url": "/coin-exchange/static/js/2.fe136b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a92cb4952557c314959",
+    "url": "/coin-exchange/static/js/main.7e507cc2.chunk.js"
+  },
+  {
+    "revision": "c9fe44687b1447b49608",
+    "url": "/coin-exchange/static/js/runtime-main.ea956e0c.js"
+  },
+  {
+    "revision": "d68962ad7caf09397217684a29f02802",
+    "url": "/coin-exchange/static/media/logo.d68962ad.png"
+  }
+]);
